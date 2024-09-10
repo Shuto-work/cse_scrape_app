0c6041671c8cac9479bda12556ef7ce16df66575
Shuto-work <shuto.work2828@gmail.com>
2024-09-10 14:26:55 +0900

    fix

--- app.py
+++ app.py
@@ -2,32 +2,28 @@ import streamlit as st
 from content import content_page
 from auth import authenticator, config, save_config
 
-def login_page():
-    if 'authentication_status' not in st.session_state:
-        st.session_state['authentication_status'] = None
-    if 'name' not in st.session_state:
-        st.session_state['name'] = None
+authenticator.login()
 
-    if st.session_state['authentication_status']:
-        authenticator.logout()
-        content_page()
-    else:
-        authenticator.login()
-        
-        if st.session_state['authentication_status'] is False:
-            st.error('Username/password is incorrect')
-        elif st.session_state['authentication_status'] is None:
-            st.warning('Please enter your username and password')
+def login_page():
+  
+  if st.session_state['authentication_status']:
+    authenticator.logout()
+    content_page()
+    
+  elif st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+  elif st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
 
         # 新規登録フォームの表示
-        try:
-            email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
-            if email_of_registered_user:
-                st.success('User registered successfully')
-                save_config()
-        except Exception as e:
-                st.error(e)
-                save_config()
+  try:
+    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+    if email_of_registered_user:
+        st.success('User registered successfully')
+        save_config()
+  except Exception as e:
+        st.error(e)
+        save_config()
 
 if __name__ == "__main__":
     login_page()
