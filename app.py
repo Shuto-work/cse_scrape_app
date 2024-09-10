43c97620170bfeea5861534e138c8b6e1d999066
Shuto-work <shuto.work2828@gmail.com>
2024-09-10 15:01:59 +0900

    fix
    fix simple

--- app.py
+++ app.py
@@ -2,36 +2,33 @@ import streamlit as st
 from content import content_page
 from auth import authenticator, config, save_config
 
-def initialize_session_state():
-    if 'authentication_status' not in st.session_state:
-        st.session_state['authentication_status'] = None
-    if 'name' not in st.session_state:
-        st.session_state['name'] = None
 
 def login_page():
-    authenticator.login(key='login_widget')
-    
+    authenticator.login()
+
     if st.session_state['authentication_status']:
-        st.rerun()  # ログイン後は自動的にページをリロードして新しい内容を表示
+        content_page()
+    elif st.session_state['authentication_status'] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state['authentication_status'] is None:
+        st.warning('Please enter your username and password')
+
 
 def register_page():
+    st.subheader("新規登録")
     try:
         email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
-            pre_authorization=False,
-            key='register_widget'
-        )
-        
+            pre_authorization=False)
         if email_of_registered_user:
             st.success('User registered successfully')
             save_config()
-            st.experimental_rerun()  # 新規登録後にページをリロードしてログインページを表示
-
+            st.button('ログインページに戻る',onclick=login_page)
     except Exception as e:
-        st.error(f'エラー: {e}')
+        st.error(e)
         save_config()
 
+
 def main():
-    initialize_session_state()
     
     if st.session_state['authentication_status']:
         content_page()  # ログイン後はコンテンツページを表示
